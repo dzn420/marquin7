28528980738b9ef7adf0d4090572f72a5d23e028
marqzxq <77423720+marqzxq@users.noreply.github.com>
2021-01-22 23:56:50 -0300

    Add files via upload

---
+++ database/menu/kerang.js
@@ -0,0 +1,22 @@
+const kerang = (prefix, botName, ownerName) => {
+	return `
+「 *${botName}* 」
+
+◪ *INFO*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Creator: ${ownerName}
+◪ *ABOUT*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+◪ *MAKER*
+  │
+  ├─ ❏ ${prefix}is
+  ├─ ❏ ${prefix}when
+  ├─ ❏ ${prefix}rate
+  └─ ❏ ${prefix}can you
+}
+exports.shells = shells
